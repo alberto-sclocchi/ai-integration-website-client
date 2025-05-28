46454de940d6a08b4c5c70887e55c08b740fcdd5
Alberto Sclocchi <albertosclocchi@Albertos-Air.attlocal.net>
2025-05-28 14:55:56 -0400

    add phone size style

--- src/components/core/NavBar.jsx
+++ src/components/core/NavBar.jsx
@@ -11,7 +11,7 @@ export default function NavBar() {
 
       <div id="links-navbar-div">
         <Link to="#about-us" className="navbar-link">About Us</Link>
-        <Link to="#projects" className="navbar-link">Projects</Link>
+        {/* <Link to="#projects" className="navbar-link">Projects</Link> */}
         <Link to="#services" className="navbar-link">Services</Link>
         <Link to="#contact-info" className="navbar-link">Contact Info</Link>
         {/* <Link to="/messages" className="navbar-link">Messages</Link> */}
